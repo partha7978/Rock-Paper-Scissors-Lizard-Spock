c4a2776d929e2a07b87bac2aa5c81adb3a7efaf9
partha7978 <muduliparthasarathi@gmail.com>
2021-05-29 17:28:37 +0530

    adding js

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
 //--------STEPS---------
 //When user click
 //generate a random choice of the computer language. 
-//It should compare the computer choice with user choice.
+//Display choices.
 //It should show a message about.
+//It should compare the computer choice with user choice.
+
 //It should update the score.
 
-function clicked(i) {
+const result = document.getElementById("result");
+let gameRules = {
+    Rock: {
+        Rock: 'draw',
+        Scissors: 'win',
+        Paper: 'lose'
+    },
+    Paper: {
+        Paper: 'draw',
+        Rock: 'win',
+        Scissors: 'lose'
+    },
+    Scissors: {
+        Scissors: 'draw',
+        Paper: 'win',
+        Rock: 'lose'
+    }
+}
+
+function clicked(input) {
     let choices = ["Rock","Paper","Scissors"];
     let randomNumber = Math.trunc(Math.random() * 3); //trunc removes the fraction part of any number and gives the int value
+    let comp_choice = choices[randomNumber];
     
-    let userChoice = document.getElementById("userChoice").innerText = `Computer choose <span>${choices[randomNumber].toUpperCase()}</span>`;   //innerHtml is use when we use to replace any tag etc. but innertext is used to update or change the content of the tag.
-    let compChoice = document.getElementById("compChoice").innerText = `You choose <span>${input.toUpperCase()}</span>``;
+    document.getElementById("userChoice").innerHTML = `Computer choose <span>${comp_choice.toUpperCase()}</span>`;   //innerHtml is use when we use to replace any tag etc. but innertext is used to update or change the content of the tag.
+    document.getElementById("compChoice").innerHTML = `You choose <span>${input.toUpperCase()}</span>`;
+
+
+    switch(gameRules[input][comp_choice]) {
+        case 'win':
+            result.innerText = `You win`;
+            break;
+        case 'lose':
+            result.innerText = `You lose`;
+            break;
+        case 'lose':
+            result.innerText = `You lose`;
+            break;   
+    }
 }
