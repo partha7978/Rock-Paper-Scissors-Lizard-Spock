1fefae629a160ad3ccfdbc785556b08e45a5ecac
partha7978 <muduliparthasarathi@gmail.com>
2021-08-28 19:27:13 +0530

    body and github dark mode part done

---
+++ scripts/darkMode.js
@@ -0,0 +1,17 @@
+const button = document.querySelector(".dark-mode");
+
+button.addEventListener("click", () => {
+    body();
+    socialButtons();
+})
+
+const body = () => {
+    let element = document.body;
+    element.classList.toggle("dark-mode-body");
+}
+
+const socialButtons = () => {
+    let button = document.querySelector(".social-btn");
+    button.classList.toggle("nav-btn-color");
+    button.classList.toggle("dark-mode-button");
+}
