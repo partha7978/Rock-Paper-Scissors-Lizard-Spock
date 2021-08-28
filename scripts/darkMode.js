596be848906d380afa5363814491cd6046519781
partha7978 <muduliparthasarathi@gmail.com>
2021-08-28 19:41:25 +0530

    nav bar text part dark mode completed

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,8 +1,10 @@
 const button = document.querySelector(".dark-mode");
+let p = document.querySelector(".p-text");
 
 button.addEventListener("click", () => {
     body();
     socialButtons();
+    navBarP();
 })
 
 const body = () => {
@@ -14,4 +16,10 @@ const socialButtons = () => {
     let button = document.querySelector(".social-btn");
     button.classList.toggle("nav-btn-color");
     button.classList.toggle("dark-mode-button");
+}
+
+const navBarP = () => {
+    p.classList.toggle("p-color");
+    p.classList.toggle("p-color-dark");
+    
 }
